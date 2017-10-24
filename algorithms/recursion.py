1886076ef2ce0d3d37fef35a7fada7e0bda42239
Jenn Lee <phoenixjenn@yahoo.com>
2017-10-24 18:55:07 -0400

    added recursion example

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -1,1 +1,12 @@
 #recursion will be in here
+
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
+
+print("Enter a number")
+y = input()
+result = fact(int(y))
+print(result)
